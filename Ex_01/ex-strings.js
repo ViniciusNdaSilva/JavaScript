72940f1efacdad549a9e514dcaf13a4ebdc1d734
Vinicius.n.silva <vinicius.nascimentodasilva@outlook.com>
2022-11-19 19:21:12 -0300

    up

---
+++ Ex_01/ex-strings.js
@@ -0,0 +1,3 @@
+console.log('Bom dia Espirito Santo');
+console.log("Boa tarde Espirito Santo");
+console.log(`Boa noite Espirito Santo`);
--- ex-strings.js
+++
@@ -1,3 +0,0 @@
-console.log ('Bom dia Espirito Santo');
-console.log ('Boa tarde Espirito Santo');
-console.log ('Boa noite Espirito Santo');
